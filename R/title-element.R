34ee4f619c329e7781759b1a33915d54ddc9c42d
ergz <erodriguez@flowwest.com>
2020-06-26 11:02:32 -0700

    change new argument name through out function

--- R/title-element.R
+++ R/title-element.R
@@ -4,8 +4,8 @@
 #' @param short_name short name of the project/dataset TODO add EDI recommendations
 #' @export 
 add_title <- function(parent_element, title, short_name) {
-  dataset$title <- title 
-  dataset$shortName <- short_name
+  parent_element$title <- title 
+  parent_element$shortName <- short_name
   
-  return(dataset)
+  return(parent_element)
 }
