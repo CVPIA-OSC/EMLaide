d3b766ac9b173333a00bd728af2c98342d2d3d6f
sadie <sgill@flowwest.com>
2020-06-29 12:05:56 -0700

    finishes test coverage for title and documentation of title argument in add_title function

--- R/title-element.R
+++ R/title-element.R
@@ -2,10 +2,17 @@
 # TODO check the requirements for short_name and add appropriate error or warnings
 #' @title Add Title Elements
 #' @param parament_element dataset or project 
-#' @param title title of the project/dataset TODO add EDI recommendations
+#' @param title The title of the project and/or dataset. A complete title is between 
+#' 7 and 20 words long and includes: What, Where, and When (and Who, if relevant).
 #' @param short_name short name of the project/dataset TODO add EDI recommendations
 #' @export 
 add_title <- function(parent_element, title, short_name) {
+  
+  title_number_of_words <- length(unlist(strsplit(title, " ")))
+  if (title_number_of_words < 7 | title_number_of_words > 20) {
+    stop("Please make sure your title is between 7 and 20 words long.")
+  }
+    
   parent_element$title <- title 
   parent_element$shortName <- short_name
   
--- tests/testthat/test-title-element.R
+++ tests/testthat/test-title-element.R
@@ -1,13 +1,21 @@
-passing_dataset <- list() %>% 
-  add_title(title = "This is 5 words or more", 
-            short_name = "This is a short name for this dataset")
-
-failing_dataset <- list() %>% 
-  add_title(title = "This is 4 words", 
-            short_name = "This is a short name for this dataset")
-
-test_that("dataset title length is at least 5 words.", {
-  expect_gte(length(unlist(strsplit(passing_dataset$title, split = " "))), 5)
+test_that('dataset title length is between 7 and 20 words long', {
+  
+  passing_dataset <- list() %>% 
+    add_title(title = "This title is at least 7 words and not over 20 words.", 
+              short_name = "This is a short name for this dataset")
+  
+  expect_gte(length(unlist(strsplit(passing_dataset$title, split = " "))), 7)
+  
+  expect_error(
+    add_title(list(), title = "Too long title lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ante ipsum, consectetur quis iaculis eget, rhoncus rutrum velit. Donec cursus massa non.",
+              short_name = "This is a short name for this dataset"),
+    "Please make sure your title is between 7 and 20 words long."
+  )
+  
+  expect_error(
+    add_title(list(), title = "Too short title.",
+              short_name = "This is a short name for this dataset"),
+    "Please make sure your title is between 7 and 20 words long."
+  )
+  
 })
-
-
