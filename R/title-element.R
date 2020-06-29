46c5a6c110e942ebefe12d185f55f73b7bb445af
sadie <sgill@flowwest.com>
2020-06-29 12:14:36 -0700

    Finishes shortName documentation for add_title function. Adds test to ensure short name is shorter than title'

--- R/title-element.R
+++ R/title-element.R
@@ -1,10 +1,8 @@
-# TODO implement error when user supplies title that is too short
-# TODO check the requirements for short_name and add appropriate error or warnings
 #' @title Add Title Elements
 #' @param parament_element dataset or project 
 #' @param title The title of the project and/or dataset. A complete title is between 
 #' 7 and 20 words long and includes: What, Where, and When (and Who, if relevant).
-#' @param short_name short name of the project/dataset TODO add EDI recommendations
+#' @param short_name Short name or nickname you use to refer to this dataset
 #' @export 
 add_title <- function(parent_element, title, short_name) {
   
