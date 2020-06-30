79bc8e77f2a823da4571379cda325d2107bedb0c
sadie <sgill@flowwest.com>
2020-06-30 13:21:04 -0700

    wraps work flow in function for build process

--- R/work-flow.R
+++ R/work-flow.R
@@ -1,14 +1,19 @@
 # WORK FLOW -----
-rmarkdown::pandoc_convert('data-raw/example_abstract.docx', to = 'markdown', 
-                          wd = getwd(), output = 'data-raw/temp.md')
-
-a <- read_file('data-raw/temp.md')
-
-dataset <- list() %>%
-  add_title(title = "OMG it is a title of sufficient length, WOW!", 
-            short_name = "shorter name dur") %>% 
-  add_abstract(a) 
-
-list(dataset = dataset) %>% 
-  as_xml() %>% 
-  xml2::write_xml('doge.xml')
+#' @title temp
+d <- function() {
+  rmarkdown::pandoc_convert('data-raw/example_abstract.docx', to = 'markdown', 
+                            wd = getwd(), output = 'data-raw/temp.md')
+  
+  a <- read_file('data-raw/temp.md')
+  
+  dataset <- list() %>%
+    add_title(title = "OMG it is a title of sufficient length, WOW!", 
+              short_name = "shorter name dur") %>% 
+    add_abstract(a) 
+  
+  list(dataset = dataset) %>% 
+    as_xml() %>% 
+    xml2::write_xml('doge.xml')
+  
+  return(NULL)
+}
