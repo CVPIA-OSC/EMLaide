9e2ba1d56500abfb647439d0f150bba9c8181650
Lizzy <egshaw@uchicago.edu>
2020-07-09 10:01:20 -0500

    Adds further documentation for the add_title function, including description, return values, and examples.

--- R/title-element.R
+++ R/title-element.R
@@ -1,8 +1,20 @@
 #' @title Add Title Elements
+#' @description adds title elements according to EML standards 
 #' @param parament_element a list representing the EML project or dataset
 #' @param title The title of the project and/or dataset. A complete title is between 
 #' 7 and 20 words long and includes: What, Where, and When (and Who, if relevant).
 #' @param short_name Short name or nickname you use to refer to this dataset. 
+#' @return the project or dataset list with title and short name appended 
+#' @examples 
+#' add_title(parent_element = list(),
+#'           title = "Eight Mile Lake Research Watershed, Carbon in Permafrost 
+#'           Experimental Heating Research (CiPEHR): Aboveground plant biomass, 2009-2017.",
+#'           short_name = "knb-lter-bnz.501.17") 
+#'           
+#' add_title(parent_element = list(), 
+#'           title = "Long-term Ground Arthropod Monitoring Dataset at Ficity, 
+#'           USA from 1998 to 2003" ,
+#'           short_name = "Arthropods")
 #' @export 
 add_title <- function(parent_element, title, short_name) {
   
