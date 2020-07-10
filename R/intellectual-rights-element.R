6ae3c16c74ad7a0cb96d9af0622aa3c53df853b5
sadie <sgill@flowwest.com>
2020-07-10 10:52:28 -0700

    adds more documentation for license function

--- R/intellectual-rights-element.R
+++ R/intellectual-rights-element.R
@@ -1,4 +1,4 @@
-#' Add Intellectual Rights
+#' Add License and Intellectual Rights
 #' @description 
 #' @param parent_element a list representing the EML project or dataset
 #' @param default_license Use "CCO" or "CCBY" as argument value to use one of the CVPIA default licenses. Supply NULL if using another license.
@@ -7,17 +7,28 @@
 #' @param license_identifier Optional if using default_license = "CCO" or "CCBY" as default values are provided.
 #' @param intellectual_rights_descripiton Optional if using default_license = "CCO" or "CCBY" as default values are provided.
 #' @details 
-#' Creative Commons
+#' Learn more about the default CVPIA licese options from Creative Commons:
+#' \itemize{
+#'  \item \href{https://creativecommons.org/publicdomain/zero/1.0/}{CCO} - The most premissive license, appropriate for data in the public domain.
+#'  \item \href{https://creativecommons.org/licenses/by/4.0/}{CC BY} - Attribution required
+#' }
+#' 
+#' To understand the difference between license choice or to use another Creative Commons license
+#' \href{https://en.wikipedia.org/wiki/Creative_Commons_license}{this} wikipedia page is helpful.
+#' 
 #' @examples 
-#' add_license(parent_element = list(), default_license = "CCO")
+#' add_license(parent_element = list()) # defaults to CCO
 #' 
 #' add_license(parent_element = list(), default_license = "CCBY")
 #' 
 #' add_license(parent_element = list(), default_license = NULL, 
-#' license_name = "MIT", license_url = "http://", license_identifier = "MIT", intellectual_rights_descripiton = "")
+#'            license_name = "Creative Commons Attribution Non Commercial Share Alike 4.0 International", 
+#'            license_url = "https://spdx.org/licenses/CC-BY-NC-SA-4.0.html", 
+#'            license_identifier = "CC-BY-NC-SA-4.0", 
+#'            intellectual_rights_descripiton = "This information is released under the Creative Commons license - Attribution - CC BY-NC-SA (https://creativecommons.org/licenses/by-nc-sa/4.0/). The consumer of these data (\"Data User\" herein) is required to cite it appropriately in any publication that results from its use. The Data User should realize that these data may be actively used by others for ongoing research and that coordination may be necessary to prevent duplicate publication. The Data User is urged to contact the authors of these data if any questions about methodology or results occur. Where appropriate, the Data User is encouraged to consider collaboration or co-authorship with the authors. The Data User should realize that misinterpretation of data may occur if used out of context of the original study. While substantial efforts are made to ensure the accuracy of data and associated documentation, complete accuracy of data sets cannot be guaranteed. All data are made available \"as is.\" The Data User should be aware, however, that data are updated periodically and it is the responsibility of the Data User to check for new versions of the data. The data authors and the repository where these data were obtained shall not be liable for damages resulting from any use or misinterpretation of the data. You may not use the material for commercial purposes and you must distribute your contributions on this same license. Thank you.&#13;")
 #' 
 #' @export
-add_license <- function(parent_element, license_name = NULL, license_url = NULL, license_identifier = NULL,
+add_license <- function(parent_element, license_name = "CCO", license_url = NULL, license_identifier = NULL,
                         intellectual_rights_descripiton = NULL) {
   
   if (default_license == "CCO") {
