d6427298fe71a04c56169eebac13241e270e6185
Lizzy <egshaw@uchicago.edu>
2020-07-14 09:32:59 -0500

    Updates documentation for the add_coverage function.

--- R/coverage-element.R
+++ R/coverage-element.R
@@ -1,15 +1,20 @@
 #' @title Add Coverage Element
-#' @description Adds coverage element #TODO
+#' @description Adds the coverage information of a dataset based off of EML standards.
 #' @param parent_element A list representing the EML project or dataset
-#' @param geographic_description #TODO
-#' @param west_bounding_coordinate #TODO
-#' @param east_bounding_coordinate #TODO
-#' @param north_bounding_coordinate #TODO
-#' @param south_bounding_coordinate #TODO
-#' @param begin_date #TODO
-#' @param end_date #TODO
+#' @param geographic_description A description of the locations	of research sites	and	areas	related	to the	data.
+#' @param west_bounding_coordinate The west cardinality limit 
+#' @param east_bounding_coordinate The east cardinality limit.
+#' @param north_bounding_coordinate The north cardinality limit.
+#' @param south_bounding_coordinate The south cardinality limit.
+#' @param begin_date The starting date for the dataset or project.
+#' @param end_date The projected or actual end date for the dataset or project. 
 #' @return The dataset or project with coverage information appended
-#' @examples #TODO
+#' @examples 
+#' add_coverage(parent_element = list(), geographic_description = "North Slope drainage basin:
+#'              Bounding box encompasses 42 drainage basins totaling the North Slope drainage basin, Alaska, USA.",
+#'              west_bounding_coordinate = "-160.594000", east_bounding_coordinate = "-134.104800",
+#'              north_bounding_coordinate = "71.238300", south_bounding_coordinate = "67.865000", 
+#'              begin_date = "1980-01-01", end_date = "2010-12-31")
 #' @export
 add_coverage <- function(parent_element, geographic_description, west_bounding_coordinate,
                          east_bounding_coordinate, north_bounding_coordinate,
