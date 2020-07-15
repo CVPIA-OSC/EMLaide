9b3f23d4dd4a848ca8a9f311655e5e70a93062b8
Lizzy <egshaw@uchicago.edu>
2020-07-15 13:13:00 -0500

    Deletes Intellectual rights xml comments.

--- R/work-flow.R
+++ R/work-flow.R
@@ -25,4 +25,23 @@ d <- function() {
   return(NULL)
 }
 
-
+#add_license(list()) %>% as_xml() %>% write_xml('doge.xml')
+# <intellectualRights>
+#   <para>This data package is released to the \"public domain\" under Creative Commons CC0 1.0
+#     \"No Rights Reserved\" (see: https://creativecommons.org/publicdomain/zero/1.0/). It is
+#     considered professional etiquette to provide attribution of the original work if this data
+#     package is shared in whole or by individual components. A generic citation is provided for
+#     this data package on the website https://portal.edirepository.org (herein \"website\") in
+#     the summary metadata page. Communication (and collaboration) with the creators of this data
+#     package is recommended to prevent duplicate research or publication. This data package (and
+#     its components) is made available \"as is\" and with no warranty of accuracy or fitness for
+#     use. The creators of this data package and the website shall not be liable for any damages
+#     resulting from misinterpretation or misuse of the data package or its components. Periodic
+#     updates of this data package may be available from the website. Thank you.&#13; 
+#   </para>
+# </intellectualRights>
+# <licensed>
+#   <licenseName>Creative Commons Zero v1.0 Universal</licenseName>
+#   <url>https://spdx.org/licenses/CC0-1.0.html</url>
+#   <identifier>CC0-1.0</identifier>
+# </licensed>
