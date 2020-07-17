847c04dff08f3cf9d9022d1a0dedc8e5ce89324a
Lizzy <egshaw@uchicago.edu>
2020-07-17 13:28:46 -0500

    resolves conversation addressing style mistakes with the stop command.

--- R/maintenance-element.R
+++ R/maintenance-element.R
@@ -12,14 +12,15 @@
 #' @export 
 add_maintenance <- function(parent_element, status, update_frequency = NULL) {
   
-  if (missing(status)){stop('Please provide the status of your project or dataset.', call. = FALSE)}
+  if (missing(status)) {stop('Please provide the status of your project or dataset.', call. = FALSE)}
   
   if(status == "complete") {
     parent_element$maintenance <- list(description = status)
   }
   
   if(status == "ongoing") {
-    if (is.null(update_frequency)){stop('Please provide the frequency of when this project or dataset is updated.', call. = FALSE)}
+    if (is.null(update_frequency)) {stop('Please provide the frequency of when this
+                                         project or dataset is updated.', call. = FALSE)}
     
     parent_element$maintenance$description = list(para = update_frequency)
   }
