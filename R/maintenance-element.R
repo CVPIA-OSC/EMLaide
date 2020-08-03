abdfd0da6b241f879a58f01fc1fb8db8a5cead7e
Lizzy <egshaw@uchicago.edu>
2020-08-03 11:40:41 -0500

    Updates documentation style for maintenance function.

--- R/maintenance-element.R
+++ R/maintenance-element.R
@@ -2,13 +2,17 @@
 #' @description Adds the maintenance information of a dataset based off of EML standards.
 #' @param parent_element A list representing the EML project or dataset.
 #' @param status Provide the status of your project or dataset as either complete or ongoing.
-#' @param update_frequency Only needed if the status of the project or dataset is ongoing. If this is the case, please provide the frequency of which the project or dataset is updated.  
+#' @param update_frequency Only needed if the status of the project or dataset is ongoing. 
+#' If this is the case, please provide the frequency of which the project or dataset is updated.  
 #' @return The dataset or project with maintenance information appended.
 #' @examples 
-#' add_maintenance(parent_element = list(), status = "complete")
+#' add_maintenance(parent_element = list(),
+#'                 status = "complete")
 #' 
-#' add_maintenance(parent_element = list(), status = "ongoing",
-#'                 update_frequency = "Data are updated annually at the end of the calendar year.")
+#' add_maintenance(parent_element = list(),
+#'                 status = "ongoing",
+#'                 update_frequency = "Data are updated 
+#'                 annually at the end of the calendar year.")
 #' @export 
 add_maintenance <- function(parent_element, status, update_frequency = NULL) {
   
