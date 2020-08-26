a49bce39416a92d31f242dc46fe53e1ce17cf244
Lizzy <egshaw@uchicago.edu>
2020-08-26 14:42:20 -0500

    updates methods function.

--- R/methods-element.R
+++ R/methods-element.R
@@ -1,70 +1,66 @@
 #' @title Add Data Collection Method Element 
 #' @description Adds the data collection method information of a dataset based off of EML standards. 
-#' @param parent_element A list representing the EML project or dataset. If being 
-#' appended to a data table rather than the complete XML output, you can make the 
-#' parent_element an empty list to be appended to the data table seperately. 
-#' @param description How this method is being conducted. 
-#' @param title Optional. What the particular method is accomplishing.
+#' @param method_file Please provide a word document with proper formatting. The 
+#' function will then append it properly. An example word document can be found in 
+#' "~/cvpiaEDIutils/data-raw/template/methods-template.docx". This file can be opened
+#' and edited to include your proper methods section. 
 #' @param instrumentation Optional. What is being used to conduct the method. 
-#' @return The dataset or project with methods information appended.
+#' @return The methods information.
 #' @examples 
-#' add_method(parent_element = list(),
-#'            title = "Climate Data",
-#'            description = "Daily temperature (maximum/minimum) and
-#'            precipitation data were obtained for each stand from 1996 to 2011
-#'            from the online PRISM Gridded Climate database (PRISM Climate Group,
-#'            Oregon State University, http://prism.oregonstate.edu, created 26 Mar 2015)
-#'            by interpolating 4km2 resolution climate data at the centroid of each
-#'            eastern hemlock stand using values from surrounding grid cell centers
-#'            and inverse-distance squared weighting.", 
+#' add_method(method_file = "~/cvpiaEDIutils/data-raw/template/methods-template.docx",
 #'            instrumentation = "Thermometer")
-#'                 
-#' # If the method has a longer description than one paragraph:
-#' description1 <- "This is the first paragraph" 
-#' description2 <- "This is the second paragraph." 
-#' description = list(description1, description2)
-#' add_method(parent_element = list(),
-#'            title = "The Data", 
-#'            description = description,
-#'            instrumentation = "The applicable instrument")
+#'  
 #' @export 
-#' 
-add_method <- function(parent_element, description, title = NULL, 
-                       instrumentation = NULL) {
+add_method <- function(methods_file, instrumentation = character(),
+                       software = NULL,
+                       sampling_file = NULL,
+                       sampling_coverage = NULL,
+                       sampling_citation = NULL,
+                       qualityControl_file = NULL) {
   
-  required_arguments <- c("description", "title", "instrumentation")
-  missing_argument_index <- which(c(missing(description), missing(title), 
-                                    missing(instrumentation)))
-  
-  if (length(missing_argument_index) > 0) {
-    methods_error <- required_arguments[missing_argument_index][1]
-    methods_error_message <- switch(methods_error, 
-                                    description = "Please provide the description of the method you are recording.",
-                                    title = "No title inputed. Provide one for easier organization.",
-                                    instrumentation = "Provide the insrumentation device used if beneficial to understanding the dataset.")
-    if (missing(description)) {
-      stop(methods_error_message, call. = FALSE)
-    } 
+  if (missing(methods_file)) {
+    stop('Please provide the document of which your methods information resides.', call. = FALSE)
   }
-  
-  if (missing(title) | missing(instrumentation)) {
-    warning(methods_error_message, call. = FALSE)
-  }
-  
-  method <- list(methodStep = list(description = list(para = description)))
-  
-  if (!is.null(title)) {
-    method$methodStep$description$title <-  title
-  }
-  if (!is.null(instrumentation)) {
-    method$methodStep$instrumentation <- instrumentation
-  } 
-
-  if (is.null(parent_element$methods)) {
-    parent_element$methods <- method
-  } else {
-    parent_element$methods <- list(parent_element$methods, method)
-  }
-  return(parent_element)
+  return(EML::set_methods(methods_file = methods_file,
+                          instrumentation = instrumentation))
 }
+
+# add_method <- function(parent_element, description, title = NULL, 
+#                        instrumentation = NULL) {
+#   
+#   required_arguments <- c("description", "title", "instrumentation")
+#   missing_argument_index <- which(c(missing(description), missing(title), 
+#                                     missing(instrumentation)))
+#   
+#   if (length(missing_argument_index) > 0) {
+#     methods_error <- required_arguments[missing_argument_index][1]
+#     methods_error_message <- switch(methods_error, 
+#                                     description = "Please provide the description of the method you are recording.",
+#                                     title = "No title inputed. Provide one for easier organization.",
+#                                     instrumentation = "Provide the insrumentation device used if beneficial to understanding the dataset.")
+#     if (missing(description)) {
+#       stop(methods_error_message, call. = FALSE)
+#     } 
+#   }
+#   
+#   if (missing(title) | missing(instrumentation)) {
+#     warning(methods_error_message, call. = FALSE)
+#   }
+#   
+#   method <- list(methodStep = list(description = list(para = description)))
+#   
+#   if (!is.null(title)) {
+#     method$methodStep$description$title <-  title
+#   }
+#   if (!is.null(instrumentation)) {
+#     method$methodStep$instrumentation <- instrumentation
+#   } 
+# 
+#   if (is.null(parent_element$methods)) {
+#     parent_element$methods <- method
+#   } else {
+#     parent_element$methods <- list(parent_element$methods, method)
+#   }
+#   return(parent_element)
+# }
 
