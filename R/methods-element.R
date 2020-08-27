c9e91283595d2cb61c2d705b3042b5f9bcf7e835
Lizzy <egshaw@uchicago.edu>
2020-08-27 11:32:51 -0500

    Updates documentation and function for methods.

--- R/methods-element.R
+++ R/methods-element.R
@@ -2,16 +2,19 @@
 #' @description Adds the data collection method information of a dataset based off of EML standards. 
 #' @param method_file Please provide a word document with proper formatting. The 
 #' function will then append it properly. An example word document can be found in 
-#' "~/cvpiaEDIutils/data-raw/template/methods-template.docx". This file can be opened
+#' "~/cvpiaEDIutils/inst/extdata/methods-template.docx". This file can be opened
 #' and edited to include your proper methods section. 
 #' @param instrumentation Optional. What is being used to conduct the method. 
 #' @return The methods information.
 #' @examples 
-#' add_method(method_file = "~/cvpiaEDIutils/data-raw/template/methods-template.docx",
+#' add_method(parent_element = list(), 
+#'            methods_file = word_example("methods-template.docx"),
 #'            instrumentation = "Thermometer")
 #'  
 #' @export 
-add_method <- function(methods_file, instrumentation = character(),
+add_method <- function(parent_element, 
+                       methods_file, 
+                       instrumentation = character(),
                        software = NULL,
                        sampling_file = NULL,
                        sampling_coverage = NULL,
@@ -21,8 +24,9 @@ add_method <- function(methods_file, instrumentation = character(),
   if (missing(methods_file)) {
     stop('Please provide the document of which your methods information resides.', call. = FALSE)
   }
-  return(EML::set_methods(methods_file = methods_file,
-                          instrumentation = instrumentation))
+  parent_element$methods <- EML::set_methods(methods_file = methods_file,
+                                             instrumentation = instrumentation)
+  return(parent_element)
 }
 
 # add_method <- function(parent_element, description, title = NULL, 
