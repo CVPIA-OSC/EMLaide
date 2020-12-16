68ec43096da8f67c981292adc2bb0a0b8bd04a8e
ErinCain <ecain@flowwest.com>
2020-12-16 09:23:39 -0800

    added better testing and documentation to add_project

--- R/project-element.R
+++ R/project-element.R
@@ -2,32 +2,51 @@
 
 #' @title Add project element 
 #' @description This function creates a project node within the parent element that 
-#' contains all the project information. 
-#' @param parent_element A list in which the project node should be nested in 
+#' contains all the project information. This function can be used in combination with add_personnel
+#' and add_funding. Add_personnel can be used to generate the project_personnel 
+#' input and add_funding can be used to generate the award_information input.  
+#' @param parent_element A list in which the project node should be nested in. 
 #' @param project_title The title of the project that the funding is awarded to. 
-#' @param award_information A list that includes that award title and the funder name 
-#' for the funding award. 
+#' @param award_information A list that includes the award title and the funder name 
+#' for the award.This list can be created by calling the add_funding function 
+#' on the funding information. It can also be created manually. 
 #' @param project_personnel A list that includes the project personnel. It must include 
-#' the first name, last name, organization, and position for this project. 
-#'
+#' the first name, last name, organization, and position for this project. This list can be created 
+#' by calling the add_personnel function on the project personnel.It can also be created manually. 
 #' @return This function returns the parent element with a new project node containing 
 #' all project information. 
 #' @examples
-#' 
-#' add_project(project_title = "my project title", 
-#'             award_information = list(title = "Money up for grabs",
-#'                                      funderName = "Bank",
-#'                                      awardNumber = "000",
+#' add_project(parent_element = parent_element, 
+#'             project_title = "my project title", 
+#'             award_information = add_funding(funder_name = "Bank",
+#'                                             funder_identifer = "Funder 1",
+#'                                             award_number = "000",
+#'                                             award_title = "Money up for grabs",
+#'                                             award_url = "awardforme.com"),
+#'             project_personnel = add_personnel(parent_element = parent_element,
+#'                                               first_name = "Smithy",
+#'                                               last_name = "Smith",
+#'                                               email = "myemail@mail.gov",
+#'                                               role = "Manager",
+#'                                               organization = "US GOV"))
+#'                                               
+#' add_project(parent_element = parent_element, 
+#'             project_title = "my project title", 
+#'             award_information = list(funderName = "Bank",
 #'                                      funderIdentifier = "Funder 1",
+#'                                      awardNumber = "000",
+#'                                      title = "Money up for grabs",
 #'                                      awardUrl = "awardforme.com"),
-#'             personnel_info = list(individualName = list(givenName = "Smithy",
-#'                                                         surName ="Smith"),
-#'                                   organizationName = "US GOV",
-#'                                   electronicMailAddress = "myemail@mail.gov",
-#'                                   role = "Manager"))
+#'             project_personnel = list(individualName = list(givenName = "Smithy",
+#'                                                            surName = "Smith"),
+#'                                      electronicMailAddress = "myemail@mail.gov",
+#'                                      role = "Manager",
+#'                                      organizationName = "US GOV"))
 
 add_project <- function(parent_element, project_title, 
                         award_information, project_personnel) {
+  
+  required_arguments <- c("project_title", "award_infomration", "project_personnel")
   
   missing_argument_index <- which(c(missing(project_title), missing(award_information),
                                     missing(project_personnel)))
@@ -35,23 +54,50 @@ add_project <- function(parent_element, project_title,
   if (length(missing_argument_index) > 0) {
     project_error <- required_arguments[missing_argument_index][1]
     project_error_message <- switch(project_error, 
-                                    project_title = "Please provide project title",
+                                    project_title = "Please provide the project title",
                                     award_infomration = "Please provide the award information.",
                                     project_personnel = "Please provide the project personnel")
+    
     if (missing(project_title) | missing(award_information) |
         missing(project_personnel)) {
       stop(project_error_message, call. = FALSE)
     }
   }
-  if (class(award_information) != "list") {
-    award_error = "Please provide a list that includes the award title and funderName."
-    stop()
-  }
-  if (class(project_personnel) != "list") {
-    award_error = "Please provide a list that includes the project personnel's name, organization, and position for this project"
-    stop()
-  }
+  required_information <- c("funderName", "title", "individualName", "organization",
+                            "role")
   
+  null_required_information <- which(c(is.null(award_information$funderName), 
+                                       is.null(award_information$title),
+                                       (is.null(project_personnel$individualName)&
+                                          is.null(project_personnel$associatedParty$individualName)),
+                                       (is.null(project_personnel$organization)&
+                                          is.null(project_personnel$associatedParty$organization)),
+                                       (is.null(project_personnel$role)&
+                                          is.null(project_personnel$associatedParty$role))))
+  
+  if (length(null_required_information) > 0) {
+    missing_requirment_error <- required_information[null_required_information][1]
+    requirment_error_message <- switch(missing_requirment_error, 
+                                       funderName = "Please provide a list that includes the funderName.",
+                                       title = "Please provide a list that includes the title for this award.",
+                                       individualName = "Please provide a name for the project personnel.",
+                                       organization = "Please provide an organization for the project personnel.",
+                                       role = "Please provide a role for the project personnel.")
+    
+    if (is.null(award_information$funderName)| is.null(award_information$title) |
+        (is.null(project_personnel$individualName) & 
+         is.null(project_personnel$associatedParty$individualName) &
+         is.null(project_personnel$creator$individualName))|
+        (is.null(project_personnel$organization) & 
+         is.null(project_personnel$associatedParty$organization) &
+         is.null(project_personnel$creator$organization)) |
+        (is.null(project_personnel$role) & 
+         is.null(project_personnel$associatedParty$role) &
+         is.null(project_personnel$creator$role))) {
+      stop(requirment_error_message, call. = FALSE)
+    }
+  }
+
   parent_element$project <- list(title = project_title,
                                  personnel = project_personnel,
                                  award = award_information)
--- tests/testthat/test-project-elements.R
+++ tests/testthat/test-project-elements.R
@@ -1,36 +1,145 @@
+parent_element <- list()
 # Tests for add project element 
 test_that('project function errors when missing mandatory identifier inputs', {
-  expect_error(add_project(award_information = list(), 
-                           project_personnel = list(),
-                           "Please provide the project title."))
+  expect_error(add_project(parent_element = parent_element,
+                           award_information = list(),
+                           project_personnel = list()),
+               "Please provide the project title")
   
-  expect_error(add_project(project_title = "my project title", 
-                           project_personnel = list(),
-                           "Please provide the funding award information."))
+  expect_error(add_project(parent_element = parent_element, 
+                           project_title = "my project title", 
+                           project_personnel = list()),
+               "Please provide the award information.")
   
-  expect_error(add_project(project_title = "my project title", 
-                           award_information = list(), 
-                           "Please provide the personnel associated with this project."))
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
+                           award_information = list()), 
+               "Please provide the project personnel")
 })
 
-test_that('arguments are of the correct type', {
-  expect_error(add_project(project_title = "my project title", 
-                           award_information = "my cool award",
-                           personnel_info = list(individualName = list(givenName = "Sue",
+# test_that('arguments are of the correct type', {
+#   expect_error(add_project(parent_element = parent_element,
+#                            project_title = "my project title", 
+#                            award_information = "my cool award",
+#                            personnel_info = list(individualName = list(givenName = "Sue",
+#                                                                        surName ="Knew"),
+#                                                  organizationName = "Something",
+#                                                  electronicMailAddress = "i@didnt.know",
+#                                                  role = "Manager")), 
+#                "Please provide a list that includes the award title and funderName.")
+#   
+#   expect_error(add_project(parent_element = parent_element,
+#                            project_title = "my project title", 
+#                            award_information = list(title = "Money up for grabs",
+#                                                     funderName = "Bank",
+#                                                     awardNumber = "000",
+#                                                     funderIdentifier = "Funder 1",
+#                                                     awardUrl = "awardforme.com"),
+#                            personnel_info = "Sue Knew"), 
+#                "Please provide a list that includes the project personnel's name, organization, and position for this project")
+# })
+
+test_that('arguments have all the nessisary required values', {
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
+                           award_information = list(title = "Money up for grabs",
+                                                    awardNumber = "000",
+                                                    funderIdentifier = "Funder 1",
+                                                    awardUrl = "awardforme.com"),
+                           project_personnel = list(individualName = list(givenName = "Sue",
                                                                        surName ="Knew"),
                                                  organizationName = "Something",
                                                  electronicMailAddress = "i@didnt.know",
-                                                 role = "Manager"), 
-                            "Please provide a list that includes the award title and funderName."))
-  expect_error(add_project(project_title = "my project title", 
+                                                 role = "Manager")), 
+               "Please provide a list that includes the funderName.")
+  
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
+                           award_information = list(funderName = "Bank",
+                                                    awardNumber = "000",
+                                                    funderIdentifier = "Funder 1",
+                                                    awardUrl = "awardforme.com"),
+                           project_personnel = list(individualName = list(givenName = "Sue",
+                                                                       surName ="Knew"),
+                                                 organizationName = "Something",
+                                                 electronicMailAddress = "i@didnt.know",
+                                                 role = "Manager")), 
+               "Please provide a list that includes the title for this award.")
+  
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
                            award_information = list(title = "Money up for grabs",
                                                     funderName = "Bank",
                                                     awardNumber = "000",
                                                     funderIdentifier = "Funder 1",
                                                     awardUrl = "awardforme.com"),
-                           personnel_info = "Sue Knew", 
-                           "Please provide a list that includes the project personnel's name, organization, and position for this project"))
+                           project_personnel = list(organizationName = "Something",
+                                                    electronicMailAddress = "i@didnt.know",
+                                                    role = "Manager")), 
+               "Please provide a name for the project personnel.")
+  
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
+                           award_information = list(title = "Money up for grabs",
+                                                    funderName = "Bank",
+                                                    awardNumber = "000",
+                                                    funderIdentifier = "Funder 1",
+                                                    awardUrl = "awardforme.com"),
+                           project_personnel = list(individualName = list(givenName = "Sue",
+                                                                          surName ="Knew"),
+                                                    electronicMailAddress = "i@didnt.know",
+                                                    role = "Manager")), 
+               "Please provide an organization for the project personnel.")
+  
+  expect_error(add_project(parent_element = parent_element,
+                           project_title = "my project title", 
+                           award_information = list(title = "Money up for grabs",
+                                                    funderName = "Bank",
+                                                    awardNumber = "000",
+                                                    funderIdentifier = "Funder 1",
+                                                    awardUrl = "awardforme.com"),
+                           project_personnel = list(individualName = list(givenName = "Sue",
+                                                                          surName ="Knew"),
+                                                    organizationName = "Something",
+                                                    electronicMailAddress = "i@didnt.know")), 
+               "Please provide a role for the project personnel.")
 })
+
+test_that('The add_project function adds the project elements', {
+  funding <- add_funding(funder_name = "National Science Foundation",
+                         funder_identifier = "http://dx.doi.org/10.13039/100000001",
+                         award_number = "1656026",
+                         award_title = "LTER: Beaufort Sea Lagoons: An Arctic Coastal Ecosystem in Transition",
+                         award_url = "https://www.nsf.gov/awardsearch/showAward?AWD_ID=1656026",
+                         funding_description = "BLE LTER is supported by the National Science Foundation under award #1656026 (2017-08-01 to 2022-07-31).")
+  
+  person <- add_personnel(parent_element = parent_element,
+                          first_name <- "Susan", 
+                          last_name <- "Susanton", 
+                          email <- "susanton@fake.com", 
+                          role <- "Data Manager", 
+                          organization <- "USFWS")
+  
+  expected_project = list(project = list(title = "This is a new project",
+                                     personnel = list(associatedParty = list(individualName = list(givenName = "Susan", 
+                                                                                                   surName = "Susanton"), 
+                                                                             electronicMailAddress = "susanton@fake.com", 
+                                                                             organizationName = "USFWS", 
+                                                                             role = "Data Manager")),
+                                     award = list(funderName = "National Science Foundation", 
+                                                  funderIdentifier = "http://dx.doi.org/10.13039/100000001", 
+                                                  awardNumber = "1656026", 
+                                                  title = "LTER: Beaufort Sea Lagoons: An Arctic Coastal Ecosystem in Transition", 
+                                                  description = "BLE LTER is supported by the National Science Foundation under award #1656026 (2017-08-01 to 2022-07-31).",
+                                                  awardUrl = "https://www.nsf.gov/awardsearch/showAward?AWD_ID=1656026")))
+  
+  expect_equal(add_project(parent_element = parent_element,
+                           project_title = "This is a new project",
+                           award_information = funding,
+                           project_personnel = person), 
+               expected_project)
+               
+})  
 
 # Tests for add funding element 
 # Tests for add_funding function 
