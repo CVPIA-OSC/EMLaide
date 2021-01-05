59eb494925e3300943f1d7b946aa6119b357f7e4
ErinCain <ecain@flowwest.com>
2021-01-05 09:18:16 -0800

    fixed weird backticks

--- R/abstract-element.R
+++ R/abstract-element.R
@@ -9,11 +9,11 @@
 #' For	a	dataset,	the	abstract	element	can	appear	at	the	resource	level	or	the	project level.		
 #' The	\code{abstract}	element	will	be	used	for	full-text	searches,	and	it	should	be	rich	with	
 #' descriptive	text.	In	particular,	descriptions	should	include	information	that	does	not	fit	into	
-#' structured	metadata,	and	focus	on	the	`"what"`,	`"when"`,	and	`"where"`	information,	general	
+#' structured	metadata,	and	focus	on	the	"what",	"when",	and	"where"	information,	general	
 #' taxonomic	information,	as	well	as	whether	the	dataset	is	ongoing	or	completed.	Some	
 #' general	methods	description	is	appropriate,	and	broad	classes	of	measured	parameters	
 #' should	also	be	included.		For	a	large	number	of	parameters,	use	categories	instead	of	listing	
-#' all	parameters	(e.g.	use	the	term	`"nutrients"`	instead	of	nitrate,	phosphate,	calcium,	etc.),	in	
+#' all	parameters	(e.g.	use	the	term	"nutrients"	instead	of	nitrate,	phosphate,	calcium,	etc.),	in	
 #' combination	with	the	parameters	that	seem	most	relevant	for	searches.
 #' @return The dataset or project with abstract appended 
 #' @examples
@@ -28,3 +28,4 @@ add_abstract <- function(parent_element, abstract) {
   return(parent_element)
 }
 
+# 
