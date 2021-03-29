9827cddcae160e02f0d2a4112751aa4b0d72765b
ErinCain <ecain@flowwest.com>
2021-03-29 14:04:52 -0700

    Adds formating to evaluate package in the test api script

--- data-raw/test-api.R
+++ data-raw/test-api.R
@@ -2,6 +2,7 @@ library(httr)
 library(jsonlite)
 library(EDIutils)
 library(xml2)
+library(tidyr)
 
 # Define variables -------------------------------------------------------------
 file_path <- "vignettes/edi.678.1.xml" 
@@ -18,26 +19,35 @@ reserve_edi_id <- function(user_id, password) {
 
 # Play around with evaluation online...try with dataset that has URL------------
 
-
-r <- httr::POST(
-  url = "https://pasta-d.lternet.edu/package/evaluate/eml",
-  config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password),
-  body = httr::upload_file(file_path)
-)
-r
-transaction_id <- httr::content(r, as = 'text', encoding = 'UTF-8')
-
-
-r <- httr::GET(
-  url = paste0("https://pasta-d.lternet.edu/package/evaluate/report/eml/",
-               transaction_id),
-  config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password)
-)
-
-report <- httr::content(r, as = 'text', encoding = 'UTF-8')
-report
-
-errors <- str_extract_all(report, "error")
-warnings <- str_extract_all(report, "warn")
-
-
+evaluate_edi_package <- function(user_id, password, file_path) {
+  r <- httr::POST(
+    url = "https://pasta-d.lternet.edu/package/evaluate/eml",
+    config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password),
+    body = httr::upload_file(file_path)
+  )
+  if (r$status_code == "201") {
+  transaction_id <- httr::content(r, as = 'text', encoding = 'UTF-8')
+  r <- httr::GET(
+    url = paste0("https://pasta-d.lternet.edu/package/evaluate/report/eml/",
+                 transaction_id),
+    config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password)
+  )
+  report <- httr::content(r, as = 'text', encoding = 'UTF-8')
+  name <- stringr::str_extract_all(report, "(?<=<name>)(.*)(?=</name>)")
+  status <- stringr::str_extract_all(report, '[:alpha:]+(?=</status>)')
+  suggestion <- stringr::str_extract_all(report, "(?<=<suggestion>)(.*)(?=</suggestion>)")
+   
+  report_df <- tibble("Status" = as_vector(flatten(status)), 
+                      "Element Checked" = as_vector(flatten(name)),
+                      "Suggestion to fix/imporve" = as_vector(flatten(suggestion)))
+  return(report_df)
+  
+  } else {
+    message("Your request to evaluate an EDI package failed,
+           please check that you entered a valid username, password, and XML document.
+           That XML document must link to a csv accessible online.
+           See more information on request status below")
+    print(r)
+  }
+  
+}
