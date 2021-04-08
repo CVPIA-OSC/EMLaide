7a4e1a5ee5f99f5be769af10568288d6611d5311
ErinCain <ecain@flowwest.com>
2021-04-08 09:07:42 -0700

    Adds inital fixes from review

--- R/api-functions.R
+++ R/api-functions.R
@@ -1,67 +1,66 @@
 # Data package identifier reservation ------------------------------------------
-#' Reserve EDI ID 
-#' @description This package returns a funder
-#' @param user_id A string that contains the user ID for the EDI data portal. If you 
-#' do not already have an EDI account you may create one on the EDI portal. \url {https://portal.edirepository.org/nis/login.jsp}
+#' Reserve EDI Data Package Identifier 
+#' @description This package reserves and returns a unique EDI number. 
+#' @param user_id EDI data portal user ID. Create an account an
+#' EDI \href{https://portal.edirepository.org/nis/login.jsp}{here}
 #' @param password A string that contains the user password for the EDI data portal. 
 #' @return This function returns a edi identifier number. 
 #' @examples 
-#' reserve_edi_id(user_id = "samuelwright", 
-#'                password = "340account")
+#' reserve_edi_id(user_id = "samuelwright")
 #' @export                
 
 reserve_edi_id <- function(user_id, password) {
-  r <-httr::POST(
+  response <-httr::POST(
     url = "https://pasta-d.lternet.edu/package/reservations/eml/edi",
-    config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password)
+    config = httr::authenticate(paste("uid=", user_id, ",o=EDI", ",dc=edirepository,dc=org"), password)
   )
-  if (r$status_code == "201") {
-  edi_number <- httr::content(r, as = 'text', encoding = 'UTF-8')
+  if (response$status_code == "201") {
+  edi_number <- httr::content(response, as = "text", encoding = "UTF-8")
   paste0("edi.", edi_number, ".1", sep = "")
 } else {
   message("Your request to reserve an EDI number failed, 
           please check that you entered a valid username and password. 
           See more information on request status below")
-  print(r)
+  print(response)
 }
 }
 
 # Evaluate EDI Data package -------------------------------------------------------
-#' Evaluate EDI Package 
-#' @description This function takes in authentication info for edi and an EML file to 
-#' be evaluated using the EDI congruency checker. This package returns a data frame that contains the status of the 
+#' Evaluate EDI Data Package 
+#' @description This function takes in authentication info for EDI and an EML file to 
+#' be evaluated using the EDI congruence checker. This package returns a data frame that contains the status of the 
 #' package. 
-#' @param user_id A string that contains the user ID for the EDI data portal. If you 
-#' do not already have an EDI account you may create one on the EDI portal. \url {https://portal.edirepository.org/nis/login.jsp}
+#' @param user_id EDI data portal user ID. Create an account an
+#' EDI \href{https://portal.edirepository.org/nis/login.jsp}{here}
 #' @param password A string that contains the user password for the EDI data portal. 
 #' @param eml_file_path A string that contains the filepath to the EML document that 
 #' contains all the data package information. A web link to the csv must be included 
 #' in the dataset information in the EML in order for a data package to be evaluated. 
 #' @return This package returns a data frame that contains the status of the 
-#' package. The data frame contains a column with the status, a column with the descriotion 
-#' of what each status is referring to, and a column that contains a suggestion of how 
-#' to fix the status or improve the package. 
+#' package. The data frame contains the following information:
+#' * The status of each check - Can be valid, info, warn, or error. Errors must be fixed before package can be uploaded to EDI. 
+#' * A description of what each status is referring to 
+#' * A suggestion of how to fix the status or improve the package. 
 #' @examples 
 #' reserve_edi_id(user_id = "samuelwright", 
-#'                password = "340account",
 #'                eml_file_path = "data/edi20.1.xml")
 #' @export   
 evaluate_edi_package <- function(user_id, password, eml_file_path) {
-  r <- httr::POST(
+  response <- httr::POST(
     url = "https://pasta-d.lternet.edu/package/evaluate/eml",
-    config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password),
+    config = httr::authenticate(paste("uid=", user_id, ",o=EDI", ",dc=edirepository,dc=org"), password),
     body = httr::upload_file(eml_file_path)
   )
   if (r$status_code == "202") {
-    transaction_id <- httr::content(r, as = 'text', encoding = 'UTF-8')
-    r <- httr::GET(
+    transaction_id <- httr::content(response, as = "text", encoding = "UTF-8")
+    response <- httr::GET(
       url = paste0("https://pasta-d.lternet.edu/package/evaluate/report/eml/",
                    transaction_id),
-      config = httr::authenticate(paste('uid=', user_id, ",o=EDI", ',dc=edirepository,dc=org'), password)
+      config = httr::authenticate(paste("uid=", user_id, ",o=EDI", ",dc=edirepository,dc=org"), password)
     )
-    report <- httr::content(r, as = 'text', encoding = 'UTF-8')
+    report <- httr::content(response, as = "text", encoding = "UTF-8")
     name <- stringr::str_extract_all(report, "(?<=<name>)(.*)(?=</name>)")
-    status <- stringr::str_extract_all(report, '[:alpha:]+(?=</status>)')
+    status <- stringr::str_extract_all(report, "[:alpha:]+(?=</status>)")
     suggestion <- stringr::str_extract_all(report, "(?<=<suggestion>)(.*)(?=</suggestion>)")
     
     report_df <- dplyr::tibble("Status" = as.vector(purrr::flatten(status)), 
@@ -73,7 +72,6 @@ evaluate_edi_package <- function(user_id, password, eml_file_path) {
            please check that you entered a valid username, password, and XML document.
            That XML document must link to a csv accessible online.
            See more information on request status below")
-    print(r)
+    print(response)
   }
-  
 }
