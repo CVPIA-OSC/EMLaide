bab45d4fb9495b29d37c5376db98dfebed3b69c2
sadie <sgill@flowwest.com>
2021-05-21 15:47:58 -0700

    moves helper functions used by data table create outside of function

--- R/data-table-element.R
+++ R/data-table-element.R
@@ -4,7 +4,7 @@
 #' @param filename Name of the file you would like added to the datatable element
 #' @param attribute_info File path to the attribute sheet that contains metadata describing all attributes for your datatable.
 #' @param datatable_description Short description of contents of datatable you are adding 
-#' @param datatable_url A URL that links to the datatable you are adding (ex: dataset on AWS public bucket)
+#' @param datatable_url A URL that links to the datatable you are adding (ex: dataset on AWS public bucket or https://raw.githubusercontent.com/)
 #' @param dataset_methods Optional metadata describing methods if there are methods that are specific to a datatable
 #' @param additional_info Option additional metadata info if there is additional metadata information that is important to the datatable 
 #' @return A data table element listed in a format to be added to a dataset element and turned into EML 
@@ -27,29 +27,6 @@ create_datatable <- function(filepath,
   attribute_list <- list()
   attribute_names <- unique(codes$attribute_name)
   
-  # Code helper function 
-  code_helper <- function(code, definitions) {
-    codeDefinition <- list(code = code, definition = definitions)
-  }
-  # Attribute helper function to input into pmap
-  attributes_and_codes <- function(attribute_name, attribute_definition, storage_type, 
-                                   measurement_scale, domain, type, units, unit_precision, 
-                                   number_type, date_time_format, date_time_precision, minimum, maximum, 
-                                   attribute_label){
-    if (domain %in% "enumerated") { 
-      definition <- list()
-      current_codes <- codes[codes$attribute_name == attribute_name, ]
-      definition$codeDefinition <- purrr::pmap(current_codes %>% select(-attribute_name), code_helper) 
-    } else {
-      definition = attribute_definition
-    }
-    new_attribute <- create_attribute(attribute_name = attribute_name, attribute_definition = attribute_definition,
-                                   storage_type = storage_type, measurement_scale = measurement_scale, 
-                                   domain = domain, definition = definition, type = type, units = units, 
-                                   unit_precision = unit_precision, number_type = number_type, 
-                                   date_time_format = date_time_format, date_time_precision = date_time_precision, 
-                                   minimum = minimum, maximum = maximum, attribute_label = attribute_label)
-  }
   attribute_list$attribute <- purrr::pmap(attribute_table, attributes_and_codes)
   
   physical <- create_physical(file_path = filepath, data_url = datatable_url)
@@ -60,6 +37,34 @@ create_datatable <- function(filepath,
                                  numberOfRecords = nrow(read.csv(filepath)))
   return(dataTable)
 }
+
+#' Code helper function 
+#' @keywords internal  
+code_helper <- function(code, definitions) {
+  codeDefinition <- list(code = code, definition = definitions)
+}
+
+#' Attribute helper function to input into pmap
+#' @keywords internal  
+attributes_and_codes <- function(attribute_name, attribute_definition, storage_type, 
+                                 measurement_scale, domain, type, units, unit_precision, 
+                                 number_type, date_time_format, date_time_precision, minimum, maximum, 
+                                 attribute_label){
+  if (domain %in% "enumerated") { 
+    definition <- list()
+    current_codes <- codes[codes$attribute_name == attribute_name, ]
+    definition$codeDefinition <- purrr::pmap(current_codes %>% select(-attribute_name), code_helper) 
+  } else {
+    definition = attribute_definition
+  }
+  new_attribute <- create_attribute(attribute_name = attribute_name, attribute_definition = attribute_definition,
+                                    storage_type = storage_type, measurement_scale = measurement_scale, 
+                                    domain = domain, definition = definition, type = type, units = units, 
+                                    unit_precision = unit_precision, number_type = number_type, 
+                                    date_time_format = date_time_format, date_time_precision = date_time_precision, 
+                                    minimum = minimum, maximum = maximum, attribute_label = attribute_label)
+}
+
 #' Add Data Table 
 #' @description Adds the data table elements to a dataset list according to EML standards. 
 #' @param parent_element A list representing the EML project or dataset.
